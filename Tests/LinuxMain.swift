6feac5569b1e621269079b31aae01b5d95ed7dbd
IL KYOUNG HWANG <sizuha@gngs.co.jp>
2020-02-18 12:54:25 +0900

    + first commit

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import SizUI_iosTests
+
+var tests = [XCTestCaseEntry]()
+tests += SizUI_iosTests.allTests()
+XCTMain(tests)
---
+++ Tests/SizUI-iosTests/SizUI_iosTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SizUI
+
+final class SizUI_iosTests: XCTestCase {
+    func testExample() {
+        
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
