13b484c318895ba83d763b46ed707628b41621d5
Sizuha <sizuha.lily@gmail.com>
2021-12-29 05:26:21 +0900

    少し改良

--- Sources/SizUI/SizUI.swift
+++ Sources/SizUI/SizUI.swift
@@ -8,83 +8,80 @@
 import Foundation
 import UIKit
 
-public class SizUI {
-    
-    enum ActionButton {
-        case `default`(_ text: String, action: (()->Void)? = nil)
-        case destrucive(_ text: String, action: (()->Void)? = nil)
-        case cancel(_ text: String, action: (()->Void)? = nil)
-    }
-
-    internal class func createActions(_ buttons: [ActionButton]) -> [UIAlertAction] {
-        var actions: [UIAlertAction] = []
-        for btn in buttons {
-            var action: UIAlertAction? = nil
-            
-            switch btn {
-            case .default(let text, action: let handler):
-                action = UIAlertAction(title: text, style: .default, handler: { _ in handler?() })
-                
-            case .cancel(let text, action: let handler):
-                action = UIAlertAction(title: text, style: .cancel, handler: { _ in handler?() })
-
-            case .destrucive(let text, action: let handler):
-                action = UIAlertAction(title: text, style: .destructive, handler: { _ in handler?() })
-            }
-            
-            if let action = action {
-                actions.append(action)
-            }
-        }
-        return actions
-    }
-    
-    class ActionSheet {
-        let builder: SizAlertBuilder
-        
-        init(title: String? = nil, message: String? = nil, buttons: [ActionButton] = []) {
-            self.builder = SizAlertBuilder(
-                title: title,
-                message: message,
-                style: .actionSheet
-            )
-            self.builder.actions = createActions(buttons)
-        }
-        
-        /*init(title: String? = nil, message: NSAttributedString, buttons: [ActionButton] = []) {
-            self.builder = SizAlertBuilder(title: title, style: .actionSheet)
-            _ = self.builder.setAttributed(message: message)
-            self.builder.actions = createActions(buttons)
-        }*/
-        
-        func show(from vc: UIViewController, animate: Bool = true, completion: (()->Void)? = nil) {
-            self.builder.show(parent: vc, animated: animate, completion: completion)
-        }
-    }
-    
-    class Alert {
-        let builder: SizAlertBuilder
-        
-        init(title: String? = nil, message: String? = nil, buttons: [ActionButton] = []) {
-            self.builder = SizAlertBuilder(
-                title: title,
-                message: message,
-                style: .alert
-            )
-            self.builder.actions = createActions(buttons)
-        }
-        
-        init(title: String? = nil, message: NSAttributedString, buttons: [ActionButton] = []) {
-            self.builder = SizAlertBuilder(title: title, style: .actionSheet)
-            _ = self.builder.setAttributed(message: message)
-            self.builder.actions = createActions(buttons)
-        }
-        
-        func show(from vc: UIViewController, animate: Bool = true, completion: (()->Void)? = nil) {
-            self.builder.show(parent: vc, animated: animate, completion: completion)
-        }
-    }
-
-    
-    
+public enum ActionButton {
+    case `default`(_ text: String, action: (()->Void)? = nil)
+    case destrucive(_ text: String, action: (()->Void)? = nil)
+    case cancel(_ text: String, action: (()->Void)? = nil)
 }
+
+func createActions(_ buttons: [ActionButton]) -> [UIAlertAction] {
+    var actions: [UIAlertAction] = []
+    for btn in buttons {
+        var action: UIAlertAction? = nil
+        
+        switch btn {
+        case .default(let text, action: let handler):
+            action = UIAlertAction(title: text, style: .default, handler: { _ in handler?() })
+            
+        case .cancel(let text, action: let handler):
+            action = UIAlertAction(title: text, style: .cancel, handler: { _ in handler?() })
+
+        case .destrucive(let text, action: let handler):
+            action = UIAlertAction(title: text, style: .destructive, handler: { _ in handler?() })
+        }
+        
+        if let action = action {
+            actions.append(action)
+        }
+    }
+    return actions
+}
+
+// MARK: ActionSheet
+public class ActionSheet {
+    let builder: SizAlertBuilder
+    
+    init(title: String? = nil, message: String? = nil, buttons: [ActionButton] = []) {
+        self.builder = SizAlertBuilder(
+            title: title,
+            message: message,
+            style: .actionSheet
+        )
+        self.builder.actions = createActions(buttons)
+    }
+    
+    /*init(title: String? = nil, message: NSAttributedString, buttons: [ActionButton] = []) {
+        self.builder = SizAlertBuilder(title: title, style: .actionSheet)
+        _ = self.builder.setAttributed(message: message)
+        self.builder.actions = createActions(buttons)
+    }*/
+    
+    func show(from vc: UIViewController, animate: Bool = true, completion: (()->Void)? = nil) {
+        self.builder.show(parent: vc, animated: animate, completion: completion)
+    }
+}
+
+// MARK: Alert
+public class Alert {
+    let builder: SizAlertBuilder
+    
+    init(title: String? = nil, message: String? = nil, buttons: [ActionButton] = []) {
+        self.builder = SizAlertBuilder(
+            title: title,
+            message: message,
+            style: .alert
+        )
+        self.builder.actions = createActions(buttons)
+    }
+    
+    init(title: String? = nil, message: NSAttributedString, buttons: [ActionButton] = []) {
+        self.builder = SizAlertBuilder(title: title, style: .actionSheet)
+        _ = self.builder.setAttributed(message: message)
+        self.builder.actions = createActions(buttons)
+    }
+    
+    func show(from vc: UIViewController, animate: Bool = true, completion: (()->Void)? = nil) {
+        self.builder.show(parent: vc, animated: animate, completion: completion)
+    }
+}
+
