824b16270ba980adc59a11475fde2858c4d14cf5
Sizuha <sizuha.lily@gmail.com>
2022-11-08 10:41:27 +0900

    Add: TextFieldUtils

---
+++ Sources/SizUI/TextFieldUtils.swift
@@ -0,0 +1,37 @@
+//
+//  TextFieldUtils.swift
+//
+//  Created by Sizuha on 2022/09/08.
+//
+
+import Foundation
+import UIKit
+
+public struct TextFieldValidation {
+    public var maxLength: Int = 0
+    public var pattern: String? = nil
+}
+
+public extension UITextField {
+    
+    func check(
+        validation: TextFieldValidation,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        let currStr = self.text! as NSString
+        let changed = currStr.replacingCharacters(in: range, with: string)
+        
+        if validation.maxLength > 0 {
+            guard changed.count <= validation.maxLength else {
+                return false
+            }
+        }
+        
+        if let patternStr = validation.pattern {
+            return changed.range(of: patternStr, options: .regularExpression) != nil
+        }
+        return true
+    }
+    
+}
